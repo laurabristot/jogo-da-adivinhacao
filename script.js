2020dbf41fdfa851016e62b787d6df0a8baf58fb
Laura Bristot <107277969+laurabristot@users.noreply.github.com>
2022-07-27 19:32:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', handleKeyDown)
+
+function handleTryClick(event) {
+  event.preventDefault()
+
+  const inputNumber = document.querySelector('#inputNumber')
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+
+    screen2.querySelector(
+      'h2'
+    ).innerText = `Acertou em ${xAttempts} tentativas!`
+  }
+
+  inputNumber.value = ''
+  xAttempts++
+}
+
+function handleResetClick() {
+  toggleScreen()
+  xAttempts = 1
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
+
+
+function handleKeyDown(e) {
+  if ((e.key == "Enter") && (screen1.classList.contains('hide'))) {
+    handleResetClick()
+  }
+}
